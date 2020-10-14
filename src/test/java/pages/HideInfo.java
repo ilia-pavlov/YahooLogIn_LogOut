ee50ab2601e5ff76b7649174147bcce5d1eed43a
3472204455ilia <68978403+3472204455ilia@users.noreply.github.com>
2020-10-13 23:36:42 -0400

    Reporter implemented

--- src/test/java/pages/HideInfo.java
+++ src/test/java/pages/HideInfo.java
@@ -2,7 +2,6 @@ package pages;
 
 public class HideInfo {
     public static final String EMAIL ="iliapavlov538@yahoo.com";
-
     public static final String  PASSWORD = "I90li90a90";
     public static final String RECIPIENT = "iliapavlov314@gmail.com";
     public static final String SUBJECT = "Massage! ";
